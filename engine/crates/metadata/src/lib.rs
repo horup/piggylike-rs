5ee6662ccaba16a84487cd89932c02711ad06f3b
Søren Alsbjerg Hørup <sah@sah.dk>
2022-08-10 10:01:28 +0200

    created metadata plugin

---
+++ engine/crates/metadata/src/lib.rs
@@ -0,0 +1,33 @@
+use core::bevy::{self as bevy, prelude::Plugin};
+use std::collections::HashMap;
+use bevy::{prelude::Handle, sprite::TextureAtlas, scene::Scene};
+
+pub type Id = u64;
+
+#[derive(Default, Clone)]
+pub struct TileDef {
+    pub solid:bool,
+    pub mesh:String
+}
+
+#[derive(Default, Clone)]
+pub struct ThingDef {
+    pub player:bool,
+    pub solid:bool,
+    pub mesh:String
+}
+
+
+#[derive(Default, Clone)]
+pub struct Metadata {
+    pub tiles:HashMap<Id, TileDef>,
+    pub things:HashMap<Id, ThingDef>,
+}
+
+pub struct MetadataPlugin;
+
+impl Plugin for MetadataPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app.insert_resource(Metadata::default());
+    }
+}
