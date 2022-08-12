c392fc0da07e07103a28ee52022129a90c8e320f
Horup <sah@sah.dk>
2022-08-12 11:45:05 +0200

    played with ui

--- engine/crates/editor/examples/editor.rs
+++ engine/crates/editor/examples/editor.rs
@@ -1,8 +1,47 @@
 use bevy::prelude::*;
+use bevy_egui::EguiPlugin;
+use editor::*;
 use smart_camera::*;
 use tilemap::*;
+use metadata::*;
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
+pub fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(MetadataPlugin)
+        .add_plugin(TilemapPlugin)
+        .add_plugin(EguiPlugin)
+        .add_plugin(EditorPlugin)
+        .add_plugin(SmartCameraPlugin)
+        .add_startup_system(setup)
+        .run();
+}
+ 
+
+
+fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut metadata:ResMut<Metadata>) {
+    metadata.tiles.insert(0, TileDef {
+        solid: false,
+        mesh: "../../../assets/bush.glb#Scene0".into(),
+    });
+    metadata.tiles.insert(1, TileDef {
+        solid: false,
+        mesh: "../../../assets/grass.glb#Scene0".into(),
+    });
+    metadata.tiles.insert(2, TileDef {
+        solid: false,
+        mesh: "../../../assets/stone_wall.glb#Scene0".into(),
+    });
+    metadata.tiles.insert(3, TileDef {
+        solid: false,
+        mesh: "../../../assets/stone.glb#Scene0".into(),
+    });
+    metadata.tiles.insert(4, TileDef {
+        solid: false,
+        mesh: "../../../assets/stone.glb#Scene0".into(),
+    });
+
+
     commands
         .spawn_bundle(Camera3dBundle {
             ..Default::default()
@@ -20,12 +59,3 @@ fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>) {
         ..Default::default()
     });
 }
-
-pub fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(SmartCameraPlugin)
-        .add_startup_system(setup)
-        .run();
-}
- 
--- engine/crates/editor/src/lib.rs
+++ engine/crates/editor/src/lib.rs
@@ -1,15 +1,90 @@
 use bevy::prelude::*;
-
-pub struct EditorPlugin;
+use bevy_egui::*;
+use egui::*;
+use tilemap::Tilemap;
 
 pub fn setup(_commands:Commands) {
 
 }
 
+#[derive(Clone, PartialEq, Eq, Hash)]
+pub enum Tool {
+    PlaceTile,
+    PlaceThing
+}
+
+impl Default for Tool {
+    fn default() -> Self {
+        Tool::PlaceTile
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct EditorUI {
+    pub tool:Tool
+}
+
+#[derive(Default)]
+pub struct Editor {
+    pub map_width:String,
+    pub map_height:String
+}
+
+pub fn menu_ui(mut context: ResMut<EguiContext>, mut editor_ui:ResMut<EditorUI>, mut editor:ResMut<Editor>) {
+    TopBottomPanel::top("top_0").show(context.ctx_mut(), |ui|{
+        menu::bar(ui, |ui| {
+            ui.menu_button("File", |ui| {
+               if ui.button("New").clicked() {
+
+               }
+               if ui.button("Save").clicked() {
+
+               }
+               if ui.button("Load").clicked() {
+                
+               }
+            });
+        });
+    });
+}
+
+pub fn tools_selection_ui(mut context: ResMut<EguiContext>, mut editor_ui:ResMut<EditorUI>) {
+    egui::Window::new("Tools").show(context.ctx_mut(), |ui| {
+        ui.radio_value(&mut editor_ui.tool, Tool::PlaceTile, "Place Tile");
+        ui.radio_value(&mut editor_ui.tool, Tool::PlaceThing, "Place Thing");
+    });
+}
+
+pub fn tiles_selection_ui(mut context: ResMut<EguiContext>) {
+    egui::Window::new("Tiles").show(context.ctx_mut(), |ui| {
+
+    });
+}
+
+pub fn properties_ui(mut context: ResMut<EguiContext>, mut tilemap:ResMut<Tilemap>, mut editor_ui:ResMut<EditorUI>, mut editor:ResMut<Editor>) {
+    egui::Window::new("Properties").show(context.ctx_mut(), |ui| {
+        ui.horizontal(|ui| {
+            ui.label("Width");
+            ui.text_edit_singleline(&mut editor.map_width);
+        });
+        ui.horizontal(|ui| {
+            ui.label("Height");
+            ui.text_edit_singleline(&mut editor.map_height);
+        });
+        if ui.button("Save Changes").clicked() {
+        }
+    });
+}
+
+pub struct EditorPlugin;
 impl Plugin for EditorPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup)
-
-        ;
+        app.insert_resource(EditorUI::default());
+        app.insert_resource(Editor::default());
+        app.add_startup_system(setup);
+        app.add_system(tiles_selection_ui.after(tools_selection_ui));
+        app.add_system(tools_selection_ui);
+        app.add_system(properties_ui.after(tiles_selection_ui));
+        app.add_system(menu_ui.before(tools_selection_ui));
     }
 }
